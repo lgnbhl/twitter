32615d7728090dabe2d613723462ff98b73c024d
gf-fluginbuhl <felix.luginbuhl@theglobalfund.org>
2021-09-06 20:05:04 +0200

    test 2

--- app.R
+++ app.R
@@ -4,7 +4,7 @@ library(shiny)
 ui <- fluidPage(
   
   # Application title
-  titlePanel("Test Old Faithful Geyser Data"),
+  titlePanel("Test 2 Old Faithful Geyser Data"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
